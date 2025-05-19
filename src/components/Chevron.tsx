f3eb11bc563746287ad55d3dff29c01794b00276
blink1197 <cgbagotsay11@gmail.com>
2025-05-20 01:23:56 +0800

    create components for chevron icons

---
+++ src/components/Chevron.tsx
@@ -0,0 +1,63 @@
+export const ChevronLeft = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+        >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+        </svg>
+    );
+};
+
+export const ChevronRight = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+        >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+        </svg>
+    );
+};
+
+export const ChevronUp = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+        >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19 15l-7-7-7 7" />
+        </svg>
+    );
+};
+
+export const ChevronDown = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+        >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M5 9l7 7 7-7" />
+        </svg>
+    );
+};
