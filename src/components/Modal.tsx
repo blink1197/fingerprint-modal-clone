340a20767681cf1cccdedbb538013cf67df13764
blink1197 <cgbagotsay11@gmail.com>
2025-05-20 15:36:20 +0800

    enable selection of visit history

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,6 +5,7 @@ import { ChevronLeft, ChevronRight, ChevronUp, ChevronDown } from "./Chevron";
 import { useVisitorData } from '@fingerprintjs/fingerprintjs-pro-react'
 import { EventsGetResponse, VisitorsResponse } from "@fingerprintjs/fingerprintjs-pro-server-api";
 import { getOrdinalWord } from "@/utils/textHelper";
+import { getTimeDifference } from "@/utils/dateHelper";
 import dynamic from "next/dynamic";
 
 
@@ -24,10 +25,20 @@ const MainContent = ({ unsealedResult, visitHistory, isLoading }: { unsealedResu
   const totalVisitCount = visitHistory?.visits.length;
   const latitude = unsealedResult?.products.ipInfo?.data?.v4?.geolocation.latitude || 0;
   const longitude = unsealedResult?.products.ipInfo?.data?.v4?.geolocation.longitude || 0;
+  const [visitIndex, setVisitIndex] = useState(0);
+
+  const handlePrevious = () => {
+    setVisitIndex((prev) => Math.max(prev - 1, 0));
+  };
+
+  const handleNext = () => {
+    setVisitIndex((prev) => Math.min(prev + 1, totalVisitCount! - 1));
+  };
 
   const MapLeaflet = dynamic(() => import('@/components/MapLeaflet').then(mod => mod.default), {
     ssr: false,
   });
+
 
   return (
     <div className={`flex border-1 border-gray-300 font-sans rounded-lg ${isLoading ? "blur-xs" : ""}`}>
@@ -46,11 +57,13 @@ const MainContent = ({ unsealedResult, visitHistory, isLoading }: { unsealedResu
           Your <span className="font-bold">current IP </span>suggests you're in
         </p>
         <p>{`${city}`}, {`${country}`}</p>
-        <p>
-          Are you sure you're not using a VPN?
-        </p>
+        {!isVPN &&
+          <p>
+            Are you sure you're not using a VPN?
+          </p>
+        }
         <p className="mt-4">
-          Curious to explore more? <span className="red-highlighted-text">Sign up</span> for a free trial and unlock all the features we offer - without any commitment.
+          Curious to explore more? <span className="red-highlighted-text underline">Sign up</span> for a free trial and unlock all the features we offer - without any commitment.
         </p>
         <p className="mt-6">
           Hope we see you soon!
@@ -68,7 +81,7 @@ const MainContent = ({ unsealedResult, visitHistory, isLoading }: { unsealedResu
             <p>Your visit summary</p>
             <p className="font-bold">You visited {`${totalVisitCount}`} time{totalVisitCount === 1 ? "" : "s"}</p>
           </div>
-          <div className="flex flex-col gap-1 p-2 pl-4 items-start justify-center border-r-0">
+          <div className="flex flex-col gap-1 p-2 pl-4 items-start justify-center sm:border-r-1 border-r-0">
             <p>Incognito</p>
             <p className="font-bold">{`${incognitoCount}`} sessions</p>
           </div>
@@ -89,51 +102,59 @@ const MainContent = ({ unsealedResult, visitHistory, isLoading }: { unsealedResu
             <div className="hidden sm:flex flex-col">
               <button
                 tabIndex={-1}
-                className="border-r-1 border-b-1 border-gray-300 w-14 text-gray-400 items-center justify-center cursor-pointer hidden sm:flex py-2">
+                className={`border-r-1 border-b-1 border-gray-300 w-14 items-center justify-center cursor-pointer hidden sm:flex py-2 ${visitIndex === 0 ? "text-gray-400" : "text-[#008080]"}`}
+                onClick={handlePrevious}
+              >
                 <ChevronUp />
               </button>
               <button
                 tabIndex={-1}
-                className="border-r-1 border-gray-300 w-14 text-gray-400 items-center justify-center cursor-pointer hidden sm:flex py-2">
+                className={`border-r-1 border-b-1 border-gray-300 w-14 items-center justify-center cursor-pointer hidden sm:flex py-2 ${visitIndex === totalVisitCount! - 1 ? "text-gray-400" : "text-[#008080]"}`}
+                onClick={handleNext}
+              >
                 <ChevronDown />
               </button>
             </div>
             <button
               tabIndex={-1}
-              className="border-r-1 border-gray-300 w-20 text-gray-400 flex items-center justify-center cursor-pointer sm:hidden">
+              className={`border-r-1 border-gray-300 w-20 flex items-center justify-center cursor-pointer sm:hidden ${visitIndex === 0 ? "text-gray-400" : "text-[#008080]"}`}
+              onClick={handlePrevious}
+            >
               <ChevronLeft />
             </button>
             <div className="flex flex-col gap-1 p-2 pl-4 items-start justify-center text-xs w-[95%] md:w-[94%]">
-              <p>Now</p>
-              <p className="font-bold flex-nowrap text-left">{`${city}`}, {`${country}`}</p>
+              <p>{getTimeDifference(Date.now(), visitHistory?.visits[visitIndex].timestamp!)}</p>
+              <p className="font-bold flex-nowrap text-left">{`${visitHistory?.visits[visitIndex].ipLocation?.city?.name || city}`}, {`${visitHistory?.visits[visitIndex].ipLocation?.country?.name || country}`}</p>
             </div>
             <div className="h-20 w-full hidden sm:flex items-center justify-center border-l-1 border-gray-300 overflow-hidden">
-              <MapLeaflet latitude={latitude!} longitude={longitude!} mapId={"map-sm"} />
+              <MapLeaflet latitude={visitHistory?.visits[visitIndex].ipLocation?.latitude! || latitude} longitude={visitHistory?.visits[visitIndex].ipLocation?.longitude! || longitude} mapId={"map-sm"} />
             </div>
             <div>
             </div>
             <button
               tabIndex={-1}
-              className="border-l-1 border-gray-300 w-20 text-gray-400 flex items-center justify-center cursor-pointer sm:hidden">
+              className={`border-l-1 border-gray-300 w-20 flex items-center justify-center cursor-pointer sm:hidden ${visitIndex === totalVisitCount! - 1 ? "text-gray-400" : "text-[#008080]"}`}
+              onClick={handleNext}
+            >
               <ChevronRight />
             </button>
           </div>
           <div className="h-20 flex sm:hidden">
-            <MapLeaflet latitude={latitude!} longitude={longitude!} mapId={"map-xs"} />
+            <MapLeaflet latitude={visitHistory?.visits[visitIndex].ipLocation?.latitude! || latitude} longitude={visitHistory?.visits[visitIndex].ipLocation?.longitude! || longitude} mapId={"map-xs"} />
           </div>
         </section>
         <section className="grid grid-cols-2 divide-x divide-y divide-gray-300  border-gray-300 text-xs sm:pl-[55px]">
           <div className="flex flex-col gap-1 p-2 pl-4 items-start justify-center border-b-1 sm:border-l-1 border-gray-300">
             <p className="font-bold">IP Address</p>
-            <p>{`${ipAddress}`}</p>
+            <p>{`${visitHistory?.visits[visitIndex].ip}`}</p>
           </div>
           <div className="flex flex-col gap-1 p-2 pl-4 items-start justify-center border-r-0 bg-green-100 border-b-1">
             <p className="font-bold">Incognito mode</p>
-            <p className="text-green-700 font-bold">{isIncognito ? "Detected" : "Not Detected"}</p>
+            <p className="text-green-700 font-bold">{visitHistory?.visits[visitIndex].incognito ? "Detected" : "Not Detected"}</p>
           </div>
           <div className="flex flex-col gap-1 p-2 pl-4 items-start justify-center border-b-0 sm:border-l-1 border-gray-300">
             <p className="font-bold">Browser</p>
-            <p>{`${browserName}`} {`${browserFullVersion}`}</p>
+            <p>{`${visitHistory?.visits[visitIndex].browserDetails.browserName}`} {`${visitHistory?.visits[visitIndex].browserDetails.browserFullVersion}`}</p>
           </div>
           <div className="flex flex-col gap-1 p-2 pl-4 items-start justify-center  border-gray-300 bg-red-100 rounded-br-lg">
             <p className="font-bold">VPN</p>
@@ -143,16 +164,6 @@ const MainContent = ({ unsealedResult, visitHistory, isLoading }: { unsealedResu
       </div>
     </div>
 
-  );
-}
-
-const DeveloperContent = () => {
-  return (
-    <div className="w-full bg-zinc-100 outline-1 outline-zinc-300 rounded-lg shadow-xl min-w-64">
-      <section>
-        here
-      </section>
-    </div>
   );
 }
 
